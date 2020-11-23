e1ce6deb86df3fa94d6a129f84e904e3079e7bc3
repl.it user <replituser@example.com>
2020-11-23 13:27:36 +0000

    Reformat summieren.cc file

--- summieren.cc
+++ summieren.cc
@@ -10,8 +10,8 @@ int main() {
   int a, b, sum, i;
   for(i=0;i<234;i++){
     Numbers >> a >> b;
-    cout<< a << ' ' << b << ' ';
+    // cout<< a << endl << b << endl;
     sum = a + b;
-    SumNum << sum << ' ';
+    SumNum << sum << endl;
   }
 }
